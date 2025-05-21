6b75acee03ad51f1463b50b3aa6c0d86cdaefb7e
Yeasin <ar9485727@gmail.com>
2025-05-21 20:34:29 +0600

    favcion and metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,6 @@
-"use client"
-
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Lenis from '@studio-freight/lenis';
-import { useEffect } from "react";
+import ClientLayout from './Clientlayout'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -16,36 +13,19 @@ const geistMono = Geist_Mono({
 });
 
 
+export const metadata = {
+  title: "EstateEdge",
+};
+
+
 export default function RootLayout({ children }) {
-
-
-   useEffect(() => {
-    const lenis = new Lenis({
-      duration: 1.2,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), 
-      smooth: true,
-    });
-
-    const raf = (time) => {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    };
-
-    requestAnimationFrame(raf);
-
-    return () => {
-      lenis.destroy();
-    };
-  }, []);
-
-
 
   return (
     <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
   );
